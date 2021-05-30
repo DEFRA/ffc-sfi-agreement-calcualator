fd6bbd4b517f064ec9188244ae1a6e8ba7f2b6e8
John Watson <johnwatson484@hotmail.com>
2021-05-30 19:47:09 +0100

    Standards added

--- app/routes/standards.js
+++ app/routes/standards.js
@@ -1,10 +1,13 @@
+const standards = require('../standards')
+
 module.exports = [{
   method: 'GET',
   path: '/standards',
   options: {
     handler: async (request, h) => {
       return h.response({
-        correlationId: request.query?.correlationId
+        correlationId: request.query?.correlationId,
+        standards
       })
     }
   }
