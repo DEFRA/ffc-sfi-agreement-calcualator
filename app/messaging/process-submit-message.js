2e2b1ead1cac501df97f90725856d878970bc155
John Watson <42574977+johnwatson484@users.noreply.github.com>
2021-06-08 18:11:44 +0100

    Add payment messages (#6)

--- app/messaging/process-submit-message.js
+++ app/messaging/process-submit-message.js
@@ -1,10 +1,14 @@
+const sendMessage = require('./send-message')
+const config = require('../config')
+
 async function processSubmitMessage (message, receiver) {
   try {
     console.info('Received submitted agreement')
+    await sendMessage(message.body, 'uk.gov.sfi.payment.request', message.correlationId, config.paymentTopic)
+    console.info('Payment requested')
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
-    await receiver.abandonMessage(message)
   }
 }
 
--- app/messaging/process-withdraw-message.js
+++ app/messaging/process-withdraw-message.js
@@ -1,10 +1,9 @@
 async function processWithdrawMessage (message, receiver) {
   try {
-    console.info(`Received withdraw agreement request, correlation Id: ${message.correlationId}`)
+    console.info('Received withdraw agreement request')
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
-    await receiver.abandonMessage(message)
   }
 }
 
