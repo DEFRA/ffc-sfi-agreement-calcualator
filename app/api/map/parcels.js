3c2de52bea7c51603ec393bd3ae53ac36fb40c13
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-06-10 09:21:09 +0100

    Query by desciption

--- app/api/map/parcels.js
+++ app/api/map/parcels.js
@@ -1,33 +1,26 @@
 const base = require('./base')
 const config = require('../../config')
-const turf = require('@turf/turf')
 
 const getParcels = async (sbi) => {
   const url = `${config.publicApi}LandParcels/MapServer/0/query?where=SBI=${sbi}&outFields=*&outSR=27700&f=geojson`
   const parcels = await base.get(url)
 
-  let center = []
-
-  if (parcels.features.length) {
-    const centroid = turf.centroid(parcels)
-    center = centroid.geometry.coordinates
-  }
   return {
-    parcels,
-    center
+    parcels
   }
 }
 
-const getParcelCovers = async (sbi, sheetId, parcelId) => {
-  const url = `${config.publicApi}LandCovers/MapServer/0/query?where=SBI=${sbi}+AND+sheet_id='${sheetId}'+AND+parcel_id='${parcelId}'&outFields=*&outSR=27700&f=geojson`
+const getParcelCovers = async (sbi, sheetId, parcelId, description) => {
+  const query = [`where=SBI=${sbi}`]
+  sheetId && query.push(`sheet_id='${sheetId}'`)
+  parcelId && query.push(`parcel_id='${parcelId}'`)
+  description && query.push(`description=${description}`)
+
+  const url = `${config.publicApi}LandCovers/MapServer/0/query?${query.join('+AND+')}&outFields=*&outSR=27700&f=geojson`
   const parcels = await base.get(url)
-  const centroid = turf.centroid(parcels)
-  const center = centroid.geometry.coordinates
+
   return {
-    parcels,
-    center,
-    totalArea: parcels.features.reduce((x, y) => x + y.properties.area_ha, 0),
-    covers: parcels.features.map(x => x.properties)
+    totalArea: parcels.features.reduce((x, y) => x + y.properties.area_ha, 0)
   }
 }
 
--- app/routes/map/parcel.js
+++ app/routes/map/parcel.js
@@ -5,9 +5,9 @@ module.exports = {
   path: '/parcel',
   options: {
     handler: async (request, h) => {
-      const { sbi, sheetId, parcelId } = request.query
-      const { parcels, center, totalArea, covers } = await getParcelCovers(sbi, sheetId, parcelId)
-      return h.response({ sbi, sheetId, parcelId, parcels, center, totalArea, covers }).code(200)
+      const { sbi, sheetId, parcelId, description } = request.query
+      const { parcels, center, totalArea, covers } = await getParcelCovers(sbi, sheetId, parcelId, description)
+      return h.response({ sbi, sheetId, parcelId, parcels, center, totalArea, covers, description }).code(200)
     }
   }
 }
