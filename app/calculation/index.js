ebaa2d92f90b9291e4e11540566fe0a62f302563
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-06-29 09:13:47 +0100

    Added buildResponse

--- app/calculation/index.js
+++ app/calculation/index.js
@@ -1,5 +1,22 @@
-const calculateAgreement = (agreement) => {
-  return 6754.61
+const buildResponse = (calculationPayload) => {
+  calculate(calculationPayload, 'soilProtection', 6)
+  calculate(calculationPayload, 'permanentGrasslandProtection', 6)
+  calculate(calculationPayload, 'moorlandGrazing', 6)
+  calculate(calculationPayload, 'livestockWelfare', 6)
+  return calculationPayload
 }
 
-module.exports = calculateAgreement
+const calculate = (calculationPayload, standardType, amount) => {
+  const standard = calculationPayload.calculation.standards[standardType].actions
+  for (const [key, value] of Object.entries(standard)) {
+    standard[key] = value.map(action => {
+      action.expression = 'x*y'
+      action.value = 616800
+      action.expression = `AREA*${amount}`
+      action.value = action.area * amount
+      return action
+    })
+  }
+}
+
+module.exports = buildResponse
