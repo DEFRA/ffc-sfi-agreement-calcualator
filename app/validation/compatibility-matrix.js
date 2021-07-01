e43cc7337dc5651934e2fd7c78b678d967a9862c
John Watson <johnwatson484@hotmail.com>
2021-07-01 20:03:21 +0100

    Draft matrix added

---
+++ app/validation/compatibility-matrix.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  option1: 'OP1',
+  option2: 'OP2',
+  category: 'valid'
+}, {
+  option1: 'OP1',
+  option2: 'OP3',
+  category: 'invalid'
+}, {
+  option1: 'OP1',
+  option2: 'OP4',
+  category: 'warning'
+}]
