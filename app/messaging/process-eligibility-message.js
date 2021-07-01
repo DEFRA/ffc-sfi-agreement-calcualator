932b9a143603e01a08452d9152c92397e95647cf
David Stone <davidjamesstone@gmail.com>
2021-07-01 11:56:06 +0100

    Update eligibility response to meet contract (#13)
    * 1.5.2
    
    * Update the cache to include correlationId

--- app/messaging/process-eligibility-message.js
+++ app/messaging/process-eligibility-message.js
@@ -3,12 +3,14 @@ const isEligible = require('../eligibility')
 
 async function processEligibilityMessage (message, receiver) {
   try {
+    const sbi = message.body.sbi
+    const correlationId = message.correlationId
     console.info('Received request for eligibility check')
-    await cache.clear('eligibility', message.correlationId)
-    await cache.set('eligibility', message.correlationId, message.body)
-    console.info(`Request for eligibility check stored in cache, correlation Id: ${message.correlationId}`)
-    await cache.update('eligibility', message.correlationId, { isEligible: isEligible(message.body.sbi) })
-    console.info(`Response available for eligibility check, correlation Id: ${message.correlationId}`)
+    await cache.clear('eligibility', correlationId)
+    await cache.set('eligibility', correlationId, message.body)
+    console.info(`Request for eligibility check stored in cache, correlation Id: ${correlationId}`)
+    await cache.update('eligibility', correlationId, { correlationId, sbi, isEligible: isEligible(sbi) })
+    console.info(`Response available for eligibility check, correlation Id: ${correlationId}`)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
