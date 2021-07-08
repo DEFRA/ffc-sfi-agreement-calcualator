036308a511497186c35f09424a7c00f08915f0c0
David Stone <davidjamesstone@gmail.com>
2021-07-08 12:47:19 +0100

    Subscribe to SBI Topic (#16)
    * 1.5.3
    
    * Subscribe to request SBI topic
    
    * Update README
    
    * Remove request-sbi API call
    
    * Update sequelize to 6.6.5

--- app/config/index.js
+++ app/config/index.js
@@ -40,6 +40,7 @@ value.validateSubscription = mqConfig.validateSubscription
 value.calculateSubscription = mqConfig.calculateSubscription
 value.submitSubscription = mqConfig.submitSubscription
 value.withdrawSubscription = mqConfig.withdrawSubscription
+value.requestSBISubscription = mqConfig.requestSBISubscription
 value.paymentTopic = mqConfig.paymentTopic
 
 value.cacheConfig = cacheConfig
