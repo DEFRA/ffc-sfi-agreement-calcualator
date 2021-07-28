f8727a5fac3caa1e7b728f34e20562ebe94392b3
fareed-mohammed <fareed.mohammed.uk@gmail.com>
2021-07-28 17:21:19 +0100

    minor fixes to integrate with apply web

--- app/routes/calculate.js
+++ app/routes/calculate.js
@@ -10,10 +10,8 @@ module.exports = [{
 
       if (correlationId) {
         const cacheData = await cache.get('calculate', correlationId)
-        if (cacheData && cacheData.paymentAmount !== undefined) {
-          const calculateResponse = buildResponse(cacheData)
-          calculateResponse.correlationId = correlationId
-          return h.response(calculateResponse).code(200)
+        if (cacheData && cacheData.paymentRates !== undefined) {          
+          return h.response({ paymentRates: cacheData.paymentRates }).code(200)
         }
       }
       return h.response(`value for ${correlationId} not in cache, try later`).code(202)
