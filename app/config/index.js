92b7a869480f3e3710eb8881c487bb046b4c9704
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-08-20 09:38:37 +0100

    Update joi validation

--- app/config/index.js
+++ app/config/index.js
@@ -8,7 +8,7 @@ const { development, production, test } = require('./constants').environments
 const schema = Joi.object({
   port: Joi.number().default(3003),
   env: Joi.string().valid(development, test, production).default(development),
-  chApiGateway: Joi.string().default('').allow('')
+  chApiGateway: Joi.string().uri().required()
 })
 
 // Build config
