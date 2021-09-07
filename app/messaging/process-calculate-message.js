d105d8937d45fdb5b38539a20f4ad712c85630b4
John Watson <johnwatson484@hotmail.com>
2021-09-07 16:37:48 +0100

    Logging removed

--- app/messaging/process-calculate-message.js
+++ app/messaging/process-calculate-message.js
@@ -1,17 +1,14 @@
 const cache = require('../cache')
 const { calculatePaymentRates } = require('../calculate')
 
-async function processCalculateMessage (message, receiver) {
+const processCalculateMessage = async (message, receiver) => {
   try {
-    console.info('Received request for calculate')
     const { body, correlationId } = message
     const { code, parcels } = body
     await cache.clear('calculate', correlationId)
     await cache.set('calculate', correlationId, body)
-    console.info(`Request for calculate stored in cache, correlation Id: ${correlationId}`)
     const paymentRates = calculatePaymentRates(code, parcels)
     await cache.update('calculate', correlationId, { paymentRates })
-    console.info(`Response available for calculate, correlation Id: ${message}`)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
