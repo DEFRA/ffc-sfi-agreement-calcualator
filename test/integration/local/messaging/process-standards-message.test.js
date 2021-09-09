f1d56f28b381efecae910a765dcacfc2cf89dbcc
John Watson <johnwatson484@hotmail.com>
2021-09-09 21:01:12 +0100

    Tests added

---
+++ test/integration/local/messaging/process-standards-message.test.js
@@ -0,0 +1,78 @@
+const mockSendMessage = jest.fn()
+jest.mock('ffc-messaging', () => {
+  return {
+    MessageSender: jest.fn().mockImplementation(() => {
+      return {
+        sendMessage: mockSendMessage,
+        closeConnection: jest.fn()
+      }
+    })
+  }
+})
+const processStandardsMessage = require('../../../../app/messaging/process-standards-message')
+jest.mock('../../../../app/api', () => {
+  return {
+    get: jest.fn().mockImplementation(() => {
+      return [{
+        id: 'SP89858277',
+        info: [{
+          code: '110',
+          name: 'Arable Land',
+          area: 0
+        },
+        {
+          code: '130',
+          name: 'Permanent Grassland',
+          area: 18205.66
+        },
+        {
+          code: '140',
+          name: 'Permanent Crops',
+          area: 0
+        }]
+      }]
+    })
+  }
+})
+jest.mock('../../../../app/cache')
+let receiver
+let message
+
+describe('process standards message', () => {
+  beforeEach(async () => {
+    receiver = {
+      completeMessage: jest.fn(),
+      abandonMessage: jest.fn()
+    }
+
+    message = {
+      correlationId: 'correlationId',
+      messageId: 'messageId',
+      body: {
+        organisationId: 1,
+        sbi: 123456789,
+        callerId: 1234567
+      }
+    }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('completes valid message', async () => {
+    await processStandardsMessage(message, receiver)
+    expect(receiver.completeMessage).toHaveBeenCalledWith(message)
+  })
+
+  test('sends session message if valid', async () => {
+    await processStandardsMessage(message, receiver)
+    expect(mockSendMessage).toHaveBeenCalled()
+  })
+
+  test('abandons invalid message', async () => {
+    message = undefined
+    await processStandardsMessage(message, receiver)
+    expect(receiver.abandonMessage).toHaveBeenCalledWith(message)
+  })
+})
