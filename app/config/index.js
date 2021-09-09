f882c0c8c6f69928028d472f0cc9be6f0203218b
John Watson <johnwatson484@hotmail.com>
2021-09-09 22:02:18 +0100

    Remove Hapi

--- app/config/index.js
+++ app/config/index.js
@@ -6,14 +6,12 @@ const { development, production, test } = require('./constants').environments
 
 // Define config schema
 const schema = Joi.object({
-  port: Joi.number().default(3003),
   env: Joi.string().valid(development, test, production).default(development),
   chApiGateway: process.env.NODE_ENV === 'test' ? Joi.string().default('').allow('') : Joi.string().uri().required()
 })
 
 // Build config
 const config = {
-  port: process.env.PORT,
   env: process.env.NODE_ENV,
   chApiGateway: process.env.CH_API_GATEWAY
 }
