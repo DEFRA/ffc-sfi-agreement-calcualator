5b06109162cee234b440639c030b152c03eb5d1f
John Watson <johnwatson484@hotmail.com>
2021-09-11 12:42:17 +0100

    Update Jest

--- test/integration/messaging/process-calculate-message.test.js
+++ test/integration/messaging/process-calculate-message.test.js
@@ -9,37 +9,13 @@ jest.mock('ffc-messaging', () => {
     })
   }
 })
-const processStandardsMessage = require('../../../../app/messaging/process-standards-message')
-jest.mock('../../../../app/api', () => {
-  return {
-    get: jest.fn().mockImplementation(() => {
-      return [{
-        id: 'SP89858277',
-        info: [{
-          code: '110',
-          name: 'Arable Land',
-          area: 0
-        },
-        {
-          code: '130',
-          name: 'Permanent Grassland',
-          area: 18205.66
-        },
-        {
-          code: '140',
-          name: 'Permanent Crops',
-          area: 0
-        }]
-      }]
-    })
-  }
-})
-jest.mock('../../../../app/cache')
-const mockCache = require('../../../../app/cache')
+const processCalculateMessage = require('../../../app/messaging/process-calculate-message')
+jest.mock('../../../app/cache')
+const mockCache = require('../../../app/cache')
 let receiver
 let message
 
-describe('process standards message', () => {
+describe('process calculate message', () => {
   beforeEach(async () => {
     receiver = {
       completeMessage: jest.fn(),
@@ -50,9 +26,10 @@ describe('process standards message', () => {
       correlationId: 'correlationId',
       messageId: 'messageId',
       body: {
-        organisationId: 1,
-        sbi: 123456789,
-        callerId: 1234567
+        code: 130,
+        parcels: [{
+          area: 100
+        }]
       }
     }
   })
@@ -62,39 +39,41 @@ describe('process standards message', () => {
   })
 
   test('completes valid message', async () => {
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(receiver.completeMessage).toHaveBeenCalledWith(message)
   })
 
   test('sends session message if valid', async () => {
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(mockSendMessage).toHaveBeenCalled()
   })
 
   test('sends session with message Id as session Id', async () => {
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(mockSendMessage.mock.calls[0][0].sessionId).toBe('messageId')
   })
 
-  test('sends message with standards', async () => {
-    await processStandardsMessage(message, receiver)
-    expect(mockSendMessage.mock.calls[0][0].body.standards).toBeDefined()
+  test('sends message with all ambition levels', async () => {
+    await processCalculateMessage(message, receiver)
+    expect(mockSendMessage.mock.calls[0][0].body.Introductory).toBeDefined()
+    expect(mockSendMessage.mock.calls[0][0].body.Intermediate).toBeDefined()
+    expect(mockSendMessage.mock.calls[0][0].body.Advanced).toBeDefined()
   })
 
   test('abandons invalid message', async () => {
     message = undefined
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(receiver.abandonMessage).toHaveBeenCalledWith(message)
   })
 
   test('processes and sets cache if no cached result', async () => {
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(mockCache.setCachedResponse).toHaveBeenCalled()
   })
 
   test('does not process and set cache if cached result', async () => {
     mockCache.getCachedResponse.mockReturnValue(true)
-    await processStandardsMessage(message, receiver)
+    await processCalculateMessage(message, receiver)
     expect(mockCache.setCachedResponse).not.toHaveBeenCalled()
   })
 })
--- test/integration/messaging/process-standards-message.test.js
+++ test/integration/messaging/process-standards-message.test.js
@@ -9,13 +9,37 @@ jest.mock('ffc-messaging', () => {
     })
   }
 })
-const processCalculateMessage = require('../../../../app/messaging/process-calculate-message')
-jest.mock('../../../../app/cache')
-const mockCache = require('../../../../app/cache')
+const processStandardsMessage = require('../../../app/messaging/process-standards-message')
+jest.mock('../../../app/api', () => {
+  return {
+    get: jest.fn().mockImplementation(() => {
+      return [{
+        id: 'SP89858277',
+        info: [{
+          code: '110',
+          name: 'Arable Land',
+          area: 0
+        },
+        {
+          code: '130',
+          name: 'Permanent Grassland',
+          area: 18205.66
+        },
+        {
+          code: '140',
+          name: 'Permanent Crops',
+          area: 0
+        }]
+      }]
+    })
+  }
+})
+jest.mock('../../../app/cache')
+const mockCache = require('../../../app/cache')
 let receiver
 let message
 
-describe('process calculate message', () => {
+describe('process standards message', () => {
   beforeEach(async () => {
     receiver = {
       completeMessage: jest.fn(),
@@ -26,10 +50,9 @@ describe('process calculate message', () => {
       correlationId: 'correlationId',
       messageId: 'messageId',
       body: {
-        code: 130,
-        parcels: [{
-          area: 100
-        }]
+        organisationId: 1,
+        sbi: 123456789,
+        callerId: 1234567
       }
     }
   })
@@ -39,41 +62,39 @@ describe('process calculate message', () => {
   })
 
   test('completes valid message', async () => {
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(receiver.completeMessage).toHaveBeenCalledWith(message)
   })
 
   test('sends session message if valid', async () => {
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(mockSendMessage).toHaveBeenCalled()
   })
 
   test('sends session with message Id as session Id', async () => {
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(mockSendMessage.mock.calls[0][0].sessionId).toBe('messageId')
   })
 
-  test('sends message with all ambition levels', async () => {
-    await processCalculateMessage(message, receiver)
-    expect(mockSendMessage.mock.calls[0][0].body.Introductory).toBeDefined()
-    expect(mockSendMessage.mock.calls[0][0].body.Intermediate).toBeDefined()
-    expect(mockSendMessage.mock.calls[0][0].body.Advanced).toBeDefined()
+  test('sends message with standards', async () => {
+    await processStandardsMessage(message, receiver)
+    expect(mockSendMessage.mock.calls[0][0].body.standards).toBeDefined()
   })
 
   test('abandons invalid message', async () => {
     message = undefined
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(receiver.abandonMessage).toHaveBeenCalledWith(message)
   })
 
   test('processes and sets cache if no cached result', async () => {
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(mockCache.setCachedResponse).toHaveBeenCalled()
   })
 
   test('does not process and set cache if cached result', async () => {
     mockCache.getCachedResponse.mockReturnValue(true)
-    await processCalculateMessage(message, receiver)
+    await processStandardsMessage(message, receiver)
     expect(mockCache.setCachedResponse).not.toHaveBeenCalled()
   })
 })
