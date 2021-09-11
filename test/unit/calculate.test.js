560381fe334f3c2f581b35053a28080901545303
John Watson <johnwatson484@hotmail.com>
2021-09-11 22:41:05 +0100

    Tests renamed

--- test/unit/calculate.test.js
+++ test/unit/calculate.test.js
@@ -1,32 +1,32 @@
 const { calculatePaymentRates } = require('../../app/calculate')
 
 describe('calculate payment rates', () => {
-  test('calculates introductory payment for arable soil', () => {
+  test('calculates introductory payment', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Introductory.paymentAmount).toBe('2600.00')
   })
 
-  test('calculates intermediate payment for arable soil', () => {
+  test('calculates intermediate payment', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Intermediate.paymentAmount).toBe('4100.00')
   })
 
-  test('calculates advanced payment for arable soil', () => {
+  test('calculates advanced payment', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Advanced.paymentAmount).toBe('6000.00')
   })
 
-  test('calculates introductory rate for arable soil', () => {
+  test('calculates introductory rate', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Introductory.rate).toBe('26.00')
   })
 
-  test('calculates intermediate rate for arable soil', () => {
+  test('calculates intermediate rate', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Intermediate.rate).toBe('41.00')
   })
 
-  test('calculates advanced rate for arable soil', () => {
+  test('calculates advanced rate', () => {
     const result = calculatePaymentRates(110, [{ area: 100 }])
     expect(result.Advanced.rate).toBe('60.00')
   })
