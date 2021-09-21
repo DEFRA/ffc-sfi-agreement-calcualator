edf445844d4100b73784ac0c3282898bf4a02d45
John Watson <johnwatson484@hotmail.com>
2021-09-21 17:09:39 +0100

    Update conversion

--- app/conversion.js
+++ app/conversion.js
@@ -1,9 +1,8 @@
 const convertToInteger = (value) => {
   const currencyArray = value.toString().split('.')
   const integer = currencyArray[0]
-  const decimal = (currencyArray[1] || '00').padEnd(2, '0')
-  const trimmedDecimal = Math.ceil(decimal / (10 * decimal.length)) * (10 * decimal.length)
-  return Number(integer + trimmedDecimal)
+  const decimal = (currencyArray[1] || '00').padEnd(2, '0').substring(0, 2)
+  return Number(integer + decimal)
 }
 
 const convertToDecimal = (value) => {
