cf477b7947a845b5f9de428a4df5b4c03e57419e
John Watson <johnwatson484@hotmail.com>
2021-09-21 21:03:35 +0100

    Fix area

--- app/calculate/calculate-total-area.js
+++ app/calculate/calculate-total-area.js
@@ -1,7 +1,7 @@
 const { convertToInteger } = require('../conversion')
 
 const calculateTotalArea = (parcels) => {
-  return parcels.reduce((a, b) => a + (convertToInteger(b.area) || 0), 0)
+  return parcels.reduce((a, b) => a + (convertToInteger(b.area || 0)), 0)
 }
 
 module.exports = calculateTotalArea
