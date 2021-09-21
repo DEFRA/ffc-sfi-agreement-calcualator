b01a054f14ade8fda1a9a84f1f500a5f6f2dd618
John Watson <johnwatson484@hotmail.com>
2021-09-21 16:53:23 +0100

    Initial rounding added

--- app/conversion.js
+++ app/conversion.js
@@ -1,8 +1,9 @@
 const convertToInteger = (value) => {
   const currencyArray = value.toString().split('.')
-  const pounds = currencyArray[0]
-  const pence = (currencyArray[1] || '00').padEnd(2, '0')
-  return Number(pounds + pence)
+  const integer = currencyArray[0]
+  const decimal = (currencyArray[1] || '00').padEnd(2, '0')
+  const trimmedDecimal = Math.ceil(decimal / (10 * decimal.length)) * (10 * decimal.length)
+  return Number(integer + trimmedDecimal)
 }
 
 const convertToDecimal = (value) => {
