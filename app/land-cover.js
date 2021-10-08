a99ac6739317314e1716b248e0290353431154b1
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-10-08 13:02:35 +0100

    updated return

--- app/land-cover.js
+++ app/land-cover.js
@@ -10,15 +10,13 @@ const getLandCover = async (organisationId, callerId) => {
 const getLandCoverArea = async (organisationId, callerId) => {
   const landCover = await get(`/lms/organisation/${organisationId}/land-covers`, callerId)
 
-  const totalArea = landCover.reduce((allTotal, parcel) => {
+  return landCover.reduce((allTotal, parcel) => {
     const sum = parcel.info.reduce((total, parcelInfo) => {
       const area = parcelInfo.code !== '000' ? parcelInfo.area : 0
       return total + convertMetresToHectares(area)
     }, 0)
     return allTotal + sum
   }, 0)
-
-  return totalArea
 }
 
 module.exports = {
