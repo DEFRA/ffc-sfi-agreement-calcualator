eec9e6e7abf6151768637493a609845133507036
John Watson <johnwatson484@hotmail.com>
2021-10-28 13:08:01 +0100

    Processing updated

--- app/messaging/process-parcel-message.js
+++ app/messaging/process-parcel-message.js
@@ -8,17 +8,17 @@ const processStandardsMessage = async (message, receiver) => {
     const { body, correlationId, messageId } = message
     const { organisationId, sbi, callerId } = message.body
 
-    const cachedResponse = await getCachedResponse(config.cacheConfig.standardsCache, body, correlationId)
+    const cachedResponse = await getCachedResponse(config.cacheConfig.parcelSpatialCache, body, correlationId)
     const standards = cachedResponse ?? { standards: await getStandards(organisationId, sbi, callerId) }
 
     if (!cachedResponse) {
-      await setCachedResponse(config.cacheConfig.standardsCache, correlationId, body, standards)
+      await setCachedResponse(config.cacheConfig.parcelSpatialCache, correlationId, body, standards)
     }
 
-    await sendMessage(standards, 'uk.gov.sfi.agreement.standards.request.response', config.standardsResponseQueue, { sessionId: messageId })
+    await sendMessage(standards, 'uk.gov.sfi.agreement.parcels.request.response', config.parcelResponseQueue, { sessionId: messageId })
     await receiver.completeMessage(message)
   } catch (err) {
-    console.error('Unable to process standards request message:', err)
+    console.error('Unable to process parcels request message:', err)
     await receiver.abandonMessage(message)
   }
 }
