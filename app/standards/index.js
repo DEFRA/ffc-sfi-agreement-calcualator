4d327c23430e52a797da92f0756332b4b5440875
John Watson <johnwatson484@hotmail.com>
2021-10-28 20:28:36 +0100

    Rename variable

--- app/standards/index.js
+++ app/standards/index.js
@@ -2,8 +2,8 @@ const calculateStandards = require('./calculate-standards')
 const { getParcels } = require('../land')
 
 const getStandards = async (organisationId, sbi, callerId) => {
-  const landCover = await getParcels(organisationId, callerId)
-  return calculateStandards(landCover)
+  const parcels = await getParcels(organisationId, callerId)
+  return calculateStandards(parcels)
 }
 
 module.exports = getStandards
