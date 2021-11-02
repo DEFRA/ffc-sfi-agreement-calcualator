6f3107bb5139d35353371ffe886984da4ab48de5
John Watson <johnwatson484@hotmail.com>
2021-11-02 10:52:50 +0000

    Fixed

--- app/land/get-parcels-standard.js
+++ app/land/get-parcels-standard.js
@@ -1,13 +1,13 @@
 const getStandards = require('../standards')
 const { getParcelStandardBlobClient, downloadParcelSpatialFile } = require('../storage')
-const { getParcelsSpatial } = require('./get-parcels-spatial')
+const getParcelsSpatial = require('./get-parcels-spatial')
 
 const getParcelsStandard = async (organisationId, sbi, callerId) => {
   const parcelsResponse = await getParcelsSpatial(organisationId, sbi)
   const standardsResponse = await getStandards(organisationId, sbi, callerId)
   const parcels = await downloadParcelSpatialFile(parcelsResponse.filename)
   for (const standard of standardsResponse.standards) {
-    standard.spatial = parcels
+    standard.spatial = JSON.parse(parcels)
     standard.spatial.features = standard.spatial.features.filter(x => standard.parcels.some(y => y.id === `${x.sheet_id}${x.parcel_id}`))
   }
 
