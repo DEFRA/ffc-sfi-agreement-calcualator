c1d760912b0bcd0e49eea85c8e85e56460738146
Fay Toward <fay.toward@rpa.gov.uk>
2021-12-10 13:21:32 +0000

    entitlement check added

--- app/legacy/entitlements.js
+++ app/legacy/entitlements.js
@@ -2,7 +2,7 @@ const { get } = require('../api')
 
 const getEntitlements = async (organisationId, callerId) => {
   const url = `/SitiAgriApi/entitlements/grouped/${organisationId}`
-  const data = await get(url, callerId)
+  const data = await get(url, callerId) ?? 0
   return data?.data.reduce((acc, { quantityOwned }) => acc + quantityOwned, 0)
 }
 
