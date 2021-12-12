7895538fc42a9bfe872680d4eb6f03455f48246d
John Watson <johnwatson484@hotmail.com>
2021-12-12 15:40:42 +0000

    Update validation

--- app/validation.js
+++ app/validation.js
@@ -6,17 +6,17 @@ const getStandards = require('./standards')
 const runValidation = async (facts) => {
   const warnings = []
 
-  const eligibilityResult = await runEligibilityRules({ identifier: facts.sbi, ...facts })
+  const eligibilityResult = await runEligibilityRules({ identifier: facts.organisation.sbi, ...facts.organisation })
   eligibilityResult.failureEvents.forEach((failure) => warnings.push({ type: 'Eligibility', detail: failure }))
 
-  const standards = await getStandards(facts.organisationId, facts.sbi, facts.callerId)
+  const standards = await getStandards(facts.organisation.organisationId, facts.organisation.sbi, facts.callerId)
 
-  for (const agreementStandard of facts.agreement.standards) {
-    const standard = standards.standards.find(x => x.code === agreementStandard.code)
-    for (const agreementLandCover of agreementStandard.landCovers) {
+  for (const agreementStandard of facts.action) {
+    const standard = standards.standards.find(x => x.code === facts.action[agreementStandard].code)
+    for (const agreementLandCover of facts.action[agreementStandard].landCovers) {
       agreementLandCover.area = convertToInteger(agreementLandCover.area)
       const standardLandCover = standard.landCovers.find(x => x.code === agreementLandCover.code)
-      const result = await runAgreementLandCoverRules({ sbi: facts.sbi, identifier: agreementLandCover.code, agreementLandCover, standardLandCover })
+      const result = await runAgreementLandCoverRules({ sbi: facts.organisation.sbi, identifier: agreementLandCover.code, agreementLandCover, standardLandCover })
       result.failureEvents.forEach((failure) => warnings.push({ type: failure.type, detail: 'Failed' }))
     }
   }
