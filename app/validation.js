5ad595dabc09d3362de740452af2132f53214bab
John Watson <johnwatson484@hotmail.com>
2021-12-12 19:25:30 +0000

    Fix

--- app/validation.js
+++ app/validation.js
@@ -12,12 +12,14 @@ const runValidation = async (facts) => {
   const standards = await getStandards(facts.organisation.organisationId, facts.organisation.sbi, facts.callerId)
 
   for (const agreementStandard in facts.action) {
-    const standard = standards.standards.find(x => x.code === facts.action[agreementStandard].code)
-    for (const agreementLandCover of facts.action[agreementStandard].landCovers) {
-      agreementLandCover.area = convertToInteger(agreementLandCover.area)
-      const standardLandCover = standard.landCovers.find(x => x.code === agreementLandCover.code)
-      const result = await runAgreementLandCoverRules({ sbi: facts.organisation.sbi, identifier: agreementLandCover.code, agreementLandCover, standardLandCover })
-      result.failureEvents.forEach((failure) => warnings.push({ type: failure.type, detail: 'Failed' }))
+    if (agreementStandard !== 'paymentAmount') {
+      const standard = standards.standards.find(x => x.code === facts.action[agreementStandard].code)
+      for (const agreementLandCover of facts.action[agreementStandard].landCovers) {
+        agreementLandCover.area = convertToInteger(agreementLandCover.area)
+        const standardLandCover = standard.landCovers.find(x => x.code === agreementLandCover.code)
+        const result = await runAgreementLandCoverRules({ sbi: facts.organisation.sbi, identifier: agreementLandCover.code, agreementLandCover, standardLandCover })
+        result.failureEvents.forEach((failure) => warnings.push({ type: failure.type, detail: 'Failed' }))
+      }
     }
   }
 
