b58f38a4514418e7bdcd4d79666180bf81644b71
Fay Toward <fay.toward@rpa.gov.uk>
2021-12-13 09:13:11 +0000

    updated

--- app/legacy/entitlements.js
+++ app/legacy/entitlements.js
@@ -2,8 +2,8 @@ const { get } = require('../api')
 
 const getEntitlements = async (organisationId, callerId) => {
   const url = `/SitiAgriApi/entitlements/grouped/${organisationId}`
-  const data = await get(url, callerId) ?? 0
-  return data?.data.reduce((acc, { quantityOwned }) => acc + quantityOwned, 0)
+  const data = await get(url, callerId)
+  return data?.data.reduce((acc, { quantityOwned }) => acc + quantityOwned, 0) ?? 0
 }
 
 module.exports = {
