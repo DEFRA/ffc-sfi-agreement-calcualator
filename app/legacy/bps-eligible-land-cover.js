d19740cc02519eae48cdef920861215377725019
John Watson <johnwatson484@hotmail.com>
2022-01-07 10:52:06 +0000

    Update BPS eligibile land cover

---
+++ app/legacy/bps-eligible-land-cover.js
@@ -0,0 +1,76 @@
+module.exports = [{
+  code: '110',
+  description: 'Arable land'
+}, {
+  code: '111',
+  description: 'Land lying fallow'
+}, {
+  code: '112',
+  description: 'Temporary grass'
+}, {
+  code: '116',
+  description: 'Catch and cover crops'
+}, {
+  code: '117',
+  description: 'Leguminous and nitrogen fixing crops'
+}, {
+  code: '118',
+  description: 'Other arable crops'
+}, {
+  code: '130',
+  description: 'Permanent grassland'
+}, {
+  code: '131',
+  description: 'Permanent grassland'
+}, {
+  code: '132',
+  description: 'Permanent grass buffer'
+}, {
+  code: '140',
+  description: 'Permanent crops'
+}, {
+  code: '141',
+  description: 'Perennial crops'
+}, {
+  code: '143',
+  description: 'Short rotation coppice'
+}, {
+  code: '210',
+  description: 'Linear features'
+}, {
+  code: '211',
+  description: 'Stone wall'
+}, {
+  code: '212',
+  description: 'Hedge'
+}, {
+  code: '213',
+  description: 'Hedgebank - Cornish hedges'
+}, {
+  code: '214',
+  description: 'Trees in a line'
+}, {
+  code: '260',
+  description: 'Heaps temporary'
+}, {
+  code: '261',
+  description: 'Heap - temporary use'
+}, {
+  code: '300',
+  description: 'Non-agricultural area'
+}, {
+  code: '370',
+  description: 'Agricultural buildings'
+}, {
+  code: '373',
+  description: 'Shelter on bare soil'
+}, {
+  code: '580',
+  description: 'Inland water'
+}, {
+  code: '581',
+  description: 'River/Stream on a boundary'
+}, {
+  code: '588',
+  description: 'Drain/Ditch/Dyke on a boundary'
+}]
