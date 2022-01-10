8130f865cdc7df442afb0e0908e9ba31d8df1c25
Marc Templeton <marcte@kainos.com>
2022-01-10 11:04:06 +0000

    moorland details added to funding options

--- app/standards/funding-options.js
+++ app/standards/funding-options.js
@@ -8,4 +8,9 @@ module.exports = [{
   name: 'improved grassland soil',
   text: 'Funding for improved grassland soil',
   hint: '£28 to £58 per hectare per year based on what work you do.'
+}, {
+  code: 'sfi-moorland',
+  name: 'moorland or rough grazing',
+  text: 'Funding for moorland or rough grazing',
+  hint: '£6.45 per hectare per year, plus a single payment of £148 per year.',
 }]
