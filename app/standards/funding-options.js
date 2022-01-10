a594d9a80d6b60693a6d9e6c8b6f40d842af0acd
Marc Templeton <marcte@kainos.com>
2022-01-10 10:57:27 +0000

    funding options text and hint added for what-funding ViewModel

--- app/standards/funding-options.js
+++ app/standards/funding-options.js
@@ -1,7 +1,11 @@
 module.exports = [{
   code: 'sfi-arable-soil',
-  name: 'arable and horticultural soil'
+  name: 'arable and horticultural soil',
+  text: 'Funding for arable and horticultural soil',
+  hint: '£22 to £40 per hectare per year based on what work you do.'
 }, {
   code: 'sfi-improved-grassland',
-  name: 'improved grassland soil'
+  name: 'improved grassland soil',
+  text: 'Funding for improved grassland soil',
+  hint: '£28 to £58 per hectare per year based on what work you do.'
 }]
