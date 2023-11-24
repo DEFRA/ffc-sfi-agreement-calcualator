8c0cdd3ed5a328000d726f8b1f5bf283d58c2bbb
John Watson <john.watson1@defra.gov.uk>
2023-11-24 16:50:02 +0000

    Update contract

--- test/contract/eligibility-response.test.js
+++ test/contract/eligibility-response.test.js
@@ -1,43 +1,52 @@
 const cache = require('../../app/cache')
+const events = require('../../app/events')
 const nock = require('nock')
-const { chApiGateway } = require('../../app/config')
+const config = require('../../app/config')
 const { MessageProviderPact } = require('@pact-foundation/pact')
 const { getEligibleOrganisations } = require('../../app/eligibility')
 const createMessage = require('../../app/messaging/create-message')
 
 describe('publishing an eligibility check response', () => {
+  let responseApimMock
   let responseOrganisationsMock
   let responseLandCover
   let responseOrganisationMock
   let responseEntitlementMock
-  const organisationId = 1234567
+  const organisationId = '1234567'
   const name = 'Title Forename LastName'
   const sbi = 123456789
   const crn = 123456789
   const token = 'token'
 
   beforeAll(async () => {
+    await events.start()
     await cache.start()
     await cache.flushAll()
 
+    responseApimMock = { token_type: 'Bearer', access_token: 'token' }
     responseOrganisationsMock = { _data: [{ id: organisationId, name, sbi }] }
     responseLandCover = [{ id: 'SJ12345678', info: [{ code: '110', name: 'Arable Land', area: 60000 }] }]
     responseOrganisationMock = { _data: { id: organisationId, name, sbi, address: { address1: 'address1', address2: 'address2', address3: 'address3', postalCode: 'postalCode' } } }
     responseEntitlementMock = { data: [{ quantityOwned: 5 }] }
 
-    nock(chApiGateway)
+    nock(config.apiConfig.apimAuthorizationUrl)
+      .persist()
+      .post('/')
+      .reply(200, responseApimMock)
+
+    nock(config.chApiGateway)
       .get('/organisation/person/3337243/summary?search=')
       .reply(200, responseOrganisationsMock)
 
-    nock(chApiGateway)
+    nock(config.chApiGateway)
       .get(`/lms/organisation/${organisationId}/land-covers`)
       .reply(200, responseLandCover)
 
-    nock(chApiGateway)
+    nock(config.chApiGateway)
       .get(`/organisation/${organisationId}`)
       .reply(200, responseOrganisationMock)
 
-    nock(chApiGateway)
+    nock(config.chApiGateway)
       .get(`/SitiAgriApi/entitlements/grouped/${organisationId}`)
       .reply(200, responseEntitlementMock)
   })
@@ -45,6 +54,7 @@ describe('publishing an eligibility check response', () => {
   afterEach(async () => {
     await cache.flushAll()
     await cache.stop()
+    await events.stop()
     nock.cleanAll()
   })
 
@@ -52,7 +62,7 @@ describe('publishing an eligibility check response', () => {
     const eligibilityResult = getEligibleOrganisations(1234657890, crn, token)
     const provider = new MessageProviderPact({
       messageProviders: {
-        'eligibility check response': () => createMessage(eligibilityResult).body
+        'a new eligibility check': () => createMessage(eligibilityResult).body
       },
       provider: 'ffc-sfi-agreement-calculator',
       consumerVersionTags: ['main', 'dev', 'test', 'preprod', 'prod'],
